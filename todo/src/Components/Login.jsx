d3e2b891b71cabc7fa95c9dbf89136c0292508df
safwannazir911 <safwannazir911@gmail.com>
2023-11-26 18:39:12 +0530

    login

--- todo/src/Components/Login.jsx
+++ todo/src/Components/Login.jsx
@@ -23,18 +23,22 @@ function Login() {
     };
 
     return (
-        <div>
+        <div className="container mt-4"> {/* Add Bootstrap container class */}
             <h2>User Login</h2>
             <form onSubmit={handleSubmit}>
-                <label>
-                    Username:
-                    <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-                </label>
-                <label>
-                    Password:
-                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                </label>
-                <button type="submit">Login</button>
+                <div className="mb-3"> {/* Add Bootstrap margin class */}
+                    <label htmlFor="username" className="form-label">
+                        Username:
+                        <input type="text" className="form-control" id="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+                    </label>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="password" className="form-label">
+                        Password:
+                        <input type="password" className="form-control" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                    </label>
+                </div>
+                <button type="submit" className="btn btn-primary">Login</button> {/* Add Bootstrap button classes */}
             </form>
         </div>
     );
